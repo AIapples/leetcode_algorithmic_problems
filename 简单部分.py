2ecc6844c5a24e6be3f2bab6c9ed790d68ce3d52
AIapples <632806735@qq.com>
2019-08-18 14:35:26 +0800

    第六题完成

--- 简单部分.py
+++ 简单部分.py
@@ -273,27 +273,67 @@ C 可以放在 D (500) 和 M (1000) 的左边，来表示 400 和 900。
 输入: "{[]}"
 输出: true
 """
+# 利用栈的方式
+def brackets_quest(s):
+    dic = {")": "(", "]": "[", "}": "{"}
+    l = []
+    if not s:
+        return True
+    for i in range(len(s)):
+        if i == 0 and s[i] in dic:
+            return False
+        else:
+            if s[i] not in dic:
+                l.append(s[i])
+            if s[i] in dic:
+                value = dic[s[i]]
+                if l and l[-1] == value:
+                    l.pop()
+                else:
+                    return False
+    if not l:
+        return True
+    else:return False
+#
+# res = brackets_quest("[([[]])]")
+# print(res)
 
+# 官方解法
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
 
+        # The stack to keep track of opening brackets.
+        stack = []
 
+        # Hash map for keeping track of mappings. This keeps the code very clean.
+        # Also makes adding more types of parenthesis easier
+        mapping = {")": "(", "}": "{", "]": "["}
 
+        # For every bracket in the expression.
+        for char in s:
 
+            # If the character is an closing bracket
+            if char in mapping:
 
+                # Pop the topmost element from the stack, if it is non empty
+                # Otherwise assign a dummy value of '#' to the top_element variable
+                top_element = stack.pop() if stack else '#'
 
+                # The mapping for the opening bracket in our hash and the top
+                # element of the stack don't match, return False
+                if mapping[char] != top_element:
+                    return False
+            else:
+                # We have an opening bracket, simply push it onto the stack.
+                stack.append(char)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+        # In the end, if the stack is empty, then we have a valid expression.
+        # The stack won't be empty for cases like ((()
+        return not stack
 
 
 
